24679abfba58bc8f19c7b2cf5d1b2a1e1919356a
Jan-Peter Voigt <jpv@elektrophon.de>
2013-02-20 15:25:25 +0100

    editionEngraver reset context count

--- lalily/lalily/edition.scm
+++ lalily/lalily/edition.scm
@@ -379,6 +379,7 @@
                                                   (display-mods)
                                                   ))
                                               ))
+                                        (set! context-count (tree-create 'context))
                                         ))))
                                )
                           `(
