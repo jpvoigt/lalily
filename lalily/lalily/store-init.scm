5b6d122d9ddfc39284b027d08cff0dfad4886b7d
Jan-Peter Voigt <jpv@elektrophon.de>
2013-05-22 11:55:09 +0200

    cue lyrics editionEngraver

--- lalily/lalily/store-init.scm
+++ lalily/lalily/store-init.scm
@@ -569,7 +569,7 @@
             $(if (ly:music? lyrics) #{
               \tag #'cued \new Lyrics \with {
                 \consists #aligncue
-                \consists \editionEngraver ##f
+                \consists \editionEngraver \musicPath #'(cue)
                 fontSize = #-2
                 \override LyricText #'font-shape = #'italic
                 \override StanzaNumber #'font-shape = #'italic
